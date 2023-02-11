c40af84249fe4be2492ffc587a288338c6a80774
Manolol <manolol@mailfence.com>
2023-02-11 15:59:20 +0100

    Add 2 formulas to calculator
    perimeter in square and rectangle
    square: P=4*a
    rectange: P= (a+b)*2

--- src/main/java/xyz/manolol/jirpychat/commands/Calculate.java
+++ src/main/java/xyz/manolol/jirpychat/commands/Calculate.java
@@ -47,7 +47,7 @@ public class Calculate extends Command{
         int selection = 0;
         
         System.out.println(ConsoleColors.CYAN_BOLD +"Please select an option!");
-        System.out.println(ConsoleColors.CYAN + " 0 - Calculate Area from side (A = a*a)\n 1 - Calculate diagonal from side (d = a*sqrt(2)" + ConsoleColors.RESET);
+        System.out.println(ConsoleColors.CYAN + " 0 - Calculate Area from side (A = a*a)\n 1 - Calculate diagonal from side (d = a*sqrt(2))\n 2 - Calculate perimeter from side (P = a*4)" + ConsoleColors.RESET);
         selection = s.nextInt();
         
         switch (selection){
@@ -56,6 +56,9 @@ public class Calculate extends Command{
                 break;
             case 1:
                 square1();
+                break;
+            case 2:
+                square2();
                 break;
             default:
                 System.out.println(ConsoleColors.RED_BOLD + "You can only select existing options." + ConsoleColors.RED + " Please try again!" + ConsoleColors.RESET);
@@ -68,7 +71,7 @@ public class Calculate extends Command{
         int selection = 0;
         
         System.out.println(ConsoleColors.CYAN_BOLD + "Please select an option!");
-        System.out.println(ConsoleColors.CYAN + "0 - Calculate Area from sides (A = a*b)\n 1 - Calculate diagonal from sides (d = sqrt(pow(a,2)+pow(b,2))" + ConsoleColors.RESET);
+        System.out.println(ConsoleColors.CYAN + "0 - Calculate Area from sides (A = a*b)\n 1 - Calculate diagonal from sides (d = sqrt(pow(a,2)+pow(b,2))\n 2 - Calculate perimeter from sides (P = 2*(a+b))" + ConsoleColors.RESET);
         selection = s.nextInt();
         
         switch (selection) {
@@ -77,6 +80,9 @@ public class Calculate extends Command{
                 break;
             case 1:
                 rectangle1();
+                break;
+            case 2:
+                rectangle2();
                 break;
             default:
                 System.out.println(ConsoleColors.RED_BOLD + "You can only select existing options." + ConsoleColors.RED + " Please try again!" + ConsoleColors.RESET);
@@ -94,6 +100,7 @@ public class Calculate extends Command{
         
         System.out.print(ConsoleColors.CYAN + "a:" + ConsoleColors.RESET);
         a = s.nextDouble();
+        
         A = a*a;
         
         System.out.println(ConsoleColors.GREEN + "A = " + A + ConsoleColors.RESET);
@@ -106,9 +113,23 @@ public class Calculate extends Command{
         
         System.out.print(ConsoleColors.CYAN + "a:" + ConsoleColors.RESET);
         a = s.nextDouble();
+        
         d = a*Math.sqrt(2);
         
         System.out.println(ConsoleColors.GREEN + "d = " + d + ConsoleColors.RESET);
+    }
+    
+    private static void square2(){
+        Scanner s = new Scanner(System.in);
+        double a = 0;
+        double P = 0;
+        
+        System.out.print(ConsoleColors.CYAN + "a:" + ConsoleColors.RESET);
+        a = s.nextDouble();
+        
+        P = a * 4;
+        
+        System.out.println(ConsoleColors.GREEN + "P = " + P + ConsoleColors.RESET);
     }
     
     private static void rectangle0(){
@@ -121,6 +142,7 @@ public class Calculate extends Command{
         a = s.nextDouble();
         System.out.print(ConsoleColors.CYAN + "b:" + ConsoleColors.RESET);
         b = s.nextDouble();
+        
         A = a*b;
         
         System.out.println(ConsoleColors.GREEN + "A = " + A + ConsoleColors.RESET);
@@ -136,8 +158,25 @@ public class Calculate extends Command{
         a = s.nextDouble();
         System.out.print(ConsoleColors.CYAN + "b:" + ConsoleColors.RESET);
         b = s.nextDouble();
+        
         d = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
         
         System.out.println(ConsoleColors.GREEN + "d = " + d + ConsoleColors.RESET);
+    }
+    
+    private static void rectangle2(){
+        Scanner s = new Scanner(System.in);
+        double a = 0;
+        double b = 0;
+        double P = 0;
+        
+        System.out.print(ConsoleColors.CYAN + "a:" + ConsoleColors.RESET);
+        a = s.nextDouble();
+        System.out.print(ConsoleColors.CYAN + "b:" + ConsoleColors.RESET);
+        b = s.nextDouble();
+        
+        P = 2 * (a+b);
+        
+        System.out.println(ConsoleColors.GREEN + "P = " + P + ConsoleColors.RESET);
     }
 }
